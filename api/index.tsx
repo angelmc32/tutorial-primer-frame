3faf0141006b6620016a1da0c006e7620014af0c
Mel <angel.melendez.c@gmail.com>
2024-03-21 12:55:17 -0600

    create root frame

--- api/index.tsx
+++ api/index.tsx
@@ -1,8 +1,8 @@
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { serveStatic } from 'frog/serve-static'
+import { Button, Frog, TextInput } from 'frog';
+import { devtools } from 'frog/dev';
+import { serveStatic } from 'frog/serve-static';
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/vercel'
+import { handle } from 'frog/vercel';
 
 // Uncomment to use Edge Runtime.
 // export const config = {
@@ -14,20 +14,15 @@ export const app = new Frog({
   basePath: '/api',
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-})
+});
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
   return c.res({
     image: (
       <div
         style={{
           alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
+          background: '#9d0772',
           backgroundSize: '100% 100%',
           display: 'flex',
           flexDirection: 'column',
@@ -50,26 +45,53 @@ app.frame('/', (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          ¡GM! En este Frame podrás acuñar uno de nuestros NFTs
+        </div>
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          🍓 🍊 🫐 🍐
+        </div>
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Creados por Cripto Street 🤙🏼
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button.Link href="https://frutero.club">El Club</Button.Link>,
+      <Button.Link href="https://zora.co/">Colección</Button.Link>,
+      <Button.Link href="https://twitter.com/Cripto_street">
+        La Artista
+      </Button.Link>,
+      <Button action="/tokens-aceptados">➡️</Button>,
     ],
-  })
-})
+  });
+});
 
 // @ts-ignore
-const isEdgeFunction = typeof EdgeFunction !== 'undefined'
-const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+const isEdgeFunction = typeof EdgeFunction !== 'undefined';
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development';
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
